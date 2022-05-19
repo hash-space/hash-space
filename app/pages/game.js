63f5a9172385b121739344bd2f25b11e80e480e0
ARM <syedrizvi@users.noreply.github.com>
2022-05-19 21:20:25 +0100

    Adding stary background and tiled background options

--- app/pages/game.js
+++ app/pages/game.js
@@ -140,9 +140,9 @@ const PLANET = [
 
 async function init(element) {
   const app = new PIXI.Application({
-    width: element.clientWidth,
-    height: element.clientHeight,
-    backgroundColor: 0x696969,
+    width: 800, // element.clientWidth,
+    height: 600, //element.clientHeight,
+    backgroundColor: 0x000000, //0x696969,
     resolution: 1,
   });
   [...element.children].forEach((child) => {
@@ -163,19 +163,103 @@ async function init(element) {
   const planet = await PIXI.Texture.fromURL('/planetTeal.png');
   const context = { asset: { ship, planet }, app };
 
+  const BGTexture = await PIXI.Texture.fromURL('/backgroundTile.png');
+
   const mainContainer = new PIXI.Container();
   const graphics = new PIXI.Graphics();
 
   // Rectangle
-  graphics.lineStyle(5, 0xfeeb77, 1);
-  graphics.beginFill(0x000);
+  graphics.lineStyle(5, 0xffffff, 1);
+  graphics.beginFill(0xf0);
   graphics.drawRect(0, 0, 2000, 2000);
   graphics.endFill();
   mainContainer.height = 2000;
   mainContainer.width = 2000;
   mainContainer.addChild(graphics);
-  viewport.addChild(mainContainer);
 
+  // Add background tiling texture to mainContainer
+  // const TilingSprite = new PIXI.TilingSprite(
+  //   BGTexture,
+  //   mainContainer.width,
+  //   mainContainer.height,
+  // );
+  // mainContainer.addChild(TilingSprite);
+
+  // ========================================
+  // Stary background START
+  const starTexture = await PIXI.Texture.fromURL('/star.png');
+
+  const starAmount = 1000;
+  let cameraZ = 0;
+  const fov = 20;
+  const baseSpeed = 0.025;
+  let speed = 0;
+  let warpSpeed = 0;
+  const starStretch = 5;
+  const starBaseSize = 0.05;
+
+  // Create the stars
+  const stars = [];
+  for (let i = 0; i < starAmount; i++) {
+      const star = {
+          sprite: new PIXI.Sprite(starTexture),
+          z: 0,
+          x: 0,
+          y: 0,
+      };
+      star.sprite.anchor.x = 0.5;
+      star.sprite.anchor.y = 0.7;
+      randomizeStar(star, true);
+      app.stage.addChild(star.sprite);
+      stars.push(star);
+  }
+
+  function randomizeStar(star, initial) {
+    star.z = initial ? Math.random() * 2000 : cameraZ + Math.random() * 1000 + 2000;
+
+    // Calculate star positions with radial random coordinate so no star hits the camera.
+    const deg = Math.random() * Math.PI * 2;
+    const distance = Math.random() * 50 + 1;
+    star.x = Math.cos(deg) * distance;
+    star.y = Math.sin(deg) * distance;
+  }
+
+  // Change flight speed every 5 seconds
+  // setInterval(() => {
+  //   warpSpeed = warpSpeed > 0 ? 0 : 1;
+  // }, 5000);
+
+  // Listen for animate update
+  app.ticker.add((delta) => {
+    // Simple easing. This should be changed to proper easing function when used for real.
+    speed += (warpSpeed - speed) / 20;
+    cameraZ += delta * 10 * (speed + baseSpeed);
+    for (let i = 0; i < starAmount; i++) {
+        const star = stars[i];
+        if (star.z < cameraZ) randomizeStar(star);
+
+        // Map star 3d position to 2d with really simple projection
+        const z = star.z - cameraZ;
+        star.sprite.x = star.x * (fov / z) * app.renderer.screen.width + app.renderer.screen.width / 2;
+        star.sprite.y = star.y * (fov / z) * app.renderer.screen.width + app.renderer.screen.height / 2;
+
+        // Calculate star scale & rotation.
+        const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
+        const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
+        const distanceCenter = Math.sqrt(dxCenter * dxCenter + dyCenter * dyCenter);
+        const distanceScale = Math.max(0, (2000 - z) / 2000);
+        star.sprite.scale.x = distanceScale * starBaseSize;
+        // Star is looking towards center so that y axis is towards center.
+        // Scale the star depending on how fast we are moving, what the stretchfactor is and depending on how far away it is from the center.
+        star.sprite.scale.y = distanceScale * starBaseSize + distanceScale * speed * starStretch * distanceCenter / app.renderer.screen.width;
+        star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
+    }
+  });
+  // Stary background END
+  // ========================================
+
+  // Add mainContainer to the viewport
+  viewport.addChild(mainContainer);
   const containerPlanets = new PIXI.Container();
 
   // app.stage.on('pointermove', (e) => {
