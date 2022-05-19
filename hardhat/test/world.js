0a8d10cc1e61db35c2c59c05d9bdd5d7ae118c76
Manuel Villing <manuel.villing@gmail.com>
2022-05-19 22:43:12 +0200

    fix for test

--- hardhat/test/world.js
+++ hardhat/test/world.js
@@ -24,13 +24,25 @@ describe('World', function () {
   });
 
   it('can get list of planets', async function () {
-    const planetIdRes1 = await world.manualCreatePlanet(1, 100, 100, 22);
+    // arrange
+    const planetIdRes1 = await world.manualCreatePlanet(1, 100, 101, 22);
     await planetIdRes1.wait();
-    const planetIdRes2 = await world.manualCreatePlanet(1, 200, 200, 23);
+    const planetIdRes2 = await world.manualCreatePlanet(1, 200, 201, 23);
     await planetIdRes2.wait();
+
+    // act
     const res = await world.getPlanets(worldId);
+
+    // assert
+    expect(res[0].planetID).to.eq(BigNumber.from(0));
     expect(res[1].planetID).to.eq(BigNumber.from(1));
     expect(res[2].planetID).to.eq(BigNumber.from(2));
-    expect(res.length).to.eq(3);
+
+    // check last planet throughout
+    expect(res[3].planetID).to.eq(BigNumber.from(3));
+    expect(res[3].xCoord).to.eq(BigNumber.from(200));
+    expect(res[3].yCoord).to.eq(BigNumber.from(201));
+    expect(res[3].planetType).to.eq(BigNumber.from(23));
+    expect(res.length).to.eq(4);
   });
 });
