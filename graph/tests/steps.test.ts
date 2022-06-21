86c08b8e7845976b28356d1c367e382de9bcf539
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-06-21 14:21:27 +0100

    fix new Array typo

--- graph/tests/steps.test.ts
+++ graph/tests/steps.test.ts
@@ -3,9 +3,9 @@ import { Address, ethereum } from "@graphprotocol/graph-ts";
 import { newMockEvent, test, assert } from "matchstick-as/assembly/index";
 import { handleStepsAdded } from '../src/players'
 
-export function createNewStepsAddedEvent(stepsTaken: i32, player: address, timestamp: i32 ): StepsAdded {
+export function createNewStepsAddedEvent(stepsTaken: i32, player: string, timestamp: i32): StepsAdded {
     let newStepsAddedEvent = changetype<StepsAdded>(newMockEvent())
-    newStepsAddedEvent.parameters = new.Array()
+    newStepsAddedEvent.parameters = new Array()
     let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(stepsTaken)) // TODO: deal with ID param properly
     let stepsTakenParam = new ethereum.EventParam("stepsTaken", ethereum.Value.fromI32(stepsTaken))
     let playerParam = new ethereum.EventParam("player", ethereum.Value.fromAddress(Address.fromString(player)))
@@ -26,7 +26,6 @@ test("Can handle StepsAdded", () => {
 
     handleStepsAdded(newStepsAddedEvent);
 
-    assert.fieldEquals("StepTrackingEntity", "50", "address", "0x8d3b5Bd6CeB7217c537411D976DB557f80E64487")
-        // I need the id apparently?
+    assert.fieldEquals("StepTrackingEntity", "50", "address", "0x8d3b5Bd6CeB7217c537411D976DB557f80E64487");
 
 })
