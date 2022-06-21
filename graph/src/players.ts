6f83d9aa06abce194d6638a2685f2964ca26f100
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-06-21 14:28:25 +0100

    adding planet conquer event handling

--- graph/src/players.ts
+++ graph/src/players.ts
@@ -1,7 +1,8 @@
 import { BigInt, Value } from "@graphprotocol/graph-ts"
 import {
   Players,
-  StepsAdded
+  StepsAdded,
+  PlanetConquer
 } from "../generated/Players/Players"
 import { StepTrackingEntity } from "../generated/schema"
 
@@ -29,30 +30,9 @@ export function handleStepsAdded(event: StepsAdded): void {
   entity.totalSteps = entity.totalSteps + event.params.stepsTaken
 
   entity.save()
-
-  // TODO: consider not using event outputs, but rather calls to the contract
-  // as below.
-  
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.NFTPRICE(...)
-  // - contract.checkContractBalance(...)s
-  // - contract.determineStartingPosition(...)
-  // - contract.indexStartingPosition(...)
-  // - contract.owner(...)
-  // - contract.players(...)
 }
+
+// export function handlePlanetConquer(event: PlanetConquer): void {
+  
+// }
+
