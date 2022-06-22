b2fb92ce1a560389f0b6cb5faf6ea2a5653a6786
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-06-22 13:36:51 +0100

    test of value in new StepsAdded event correct

--- graph/tests/steps.test.ts
+++ graph/tests/steps.test.ts
@@ -2,12 +2,12 @@ import { StepsAdded } from '../generated/Players/Players'
 import { Address, ethereum } from "@graphprotocol/graph-ts";
 import { newMockEvent, test, assert, logStore } from "matchstick-as/assembly/index";
 import { handleStepsAdded } from '../src/players'
+import { StepTrackingEntity } from "../generated/schema"
 
 export function createNewStepsAddedEvent(stepsTaken: i32, player: string, timestamp: i32): StepsAdded {
     let newStepsAddedEvent = changetype<StepsAdded>(newMockEvent())
     newStepsAddedEvent.parameters = new Array()
-    // let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(stepsTaken)) // TODO: deal with ID param properly
-        // TODO: update to use the address, which the StepTrackingEntity uses for ID
+    // let idParam = new ethereum.EventParam("id", ethereum.Value.fromAddress(Address.fromString(player)))
     let stepsTakenParam = new ethereum.EventParam("stepsTaken", ethereum.Value.fromI32(stepsTaken))
     let playerParam = new ethereum.EventParam("player", ethereum.Value.fromAddress(Address.fromString(player)))
     let timestampParam = new ethereum.EventParam("timestamp", ethereum.Value.fromI32(timestamp))
@@ -21,17 +21,19 @@ export function createNewStepsAddedEvent(stepsTaken: i32, player: string, timest
 } 
 
 
-
 test("Can handle StepsAdded", () => {
-    let newStepsAddedEvent = createNewStepsAddedEvent(5, "0x8d3b5Bd6CeB7217c537411D976DB557f80E64487", 5);
+    let newStepsAddedEvent = createNewStepsAddedEvent(5, "0xa16081f360e3847006db660bae1c6d1b2e17ec2a", 5);
 
     handleStepsAdded(newStepsAddedEvent);
 
     logStore();
 
-    assert.equals(ethereum.Value.fromI32(1),ethereum.Value.fromI32(1))
+    assert.fieldEquals("StepTrackingEntity", "0xa16081f360e3847006db660bae1c6d1b2e17ec2a", "totalSteps", "5")
+
+    // assert.equals(ethereum.Value.fromI32(1),ethereum.Value.fromI32(1))
     // assert.equals(ethereum.Value.fromI32(StepTrackingEntity.totalSteps.data), ethereum.Value.fromI32(5));
 
-    // how to access stuff in store?
-
 })
+
+
+// TODO: add further tests for graph logic
