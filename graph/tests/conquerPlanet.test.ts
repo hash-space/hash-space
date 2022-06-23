8b97ccf9bfdf19a170ac6dd3096ca7b8dccd32fa
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2022-06-23 10:59:15 +0100

    add test multiple yield handling

--- graph/tests/conquerPlanet.test.ts
+++ graph/tests/conquerPlanet.test.ts
@@ -25,5 +25,28 @@ test("Can handle planet conquer event", () => {
     logStore();
 
     assert.fieldEquals("PlanetConquerEntity", "0xa16081f360e3847006db660bae1c6d1b2e17ec2a", "totalYield", "10");
+    assert.fieldEquals("PlanetConquerEntity", "0xa16081f360e3847006db660bae1c6d1b2e17ec2a", "id", "0xa16081f360e3847006db660bae1c6d1b2e17ec2a");
+    assert.fieldEquals("PlanetConquerEntity", "0xa16081f360e3847006db660bae1c6d1b2e17ec2a", "numSyncs", "1");
+
     clearStore();
+})
+
+test("Tallies yield amount correctly", () => {
+    // arrange
+    let newStepsAddedEvent1 = createPlanetConquerEvent("0xa16081f360e3847006db660bae1c6d1b2e17ec2a", 25, 1);
+    let newStepsAddedEvent2 = createPlanetConquerEvent("0xa16081f360e3847006db660bae1c6d1b2e17ec2a", 45, 1);
+    let newStepsAddedEvent3 = createPlanetConquerEvent("0xa16081f360e3847006db660bae1c6d1b2e17ec2a", 50, 1);
+
+    // act
+    handlePlanetConquer(newStepsAddedEvent1);
+    handlePlanetConquer(newStepsAddedEvent2);
+    handlePlanetConquer(newStepsAddedEvent3);
+    logStore();
+
+    // assert
+    assert.fieldEquals("PlanetConquerEntity", "0xa16081f360e3847006db660bae1c6d1b2e17ec2a", "totalYield", "120");
+    assert.fieldEquals("PlanetConquerEntity", "0xa16081f360e3847006db660bae1c6d1b2e17ec2a", "numSyncs", "3");
+
+    clearStore();
+
 })
